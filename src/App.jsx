669e60292a52b8d74b67403d9c326aa6a2f8e169
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-16 23:50:56 +0000

    feat: update App component with Star Wars: Episode V details and storyline

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 function App() {
+  // document.querySelector("button").addEventListener("click", () => {
+  //   alert("Darth Vader is Luke Skywalker’s father!");
+  // });
+
   return (
     <>
-      <h1>App</h1>
+      <h1>Star Wars: Episode V</h1>
+      <h2>Year of production: 1980</h2>
+      <h2>Storyline:</h2>
+      <p>
+        In Star Wars: Episode V - The Empire Strikes Back, the Rebel Alliance
+        faces a devastating attack on their base on Hoth by the Empire. Luke
+        Skywalker, guided by a vision of Obi-Wan Kenobi, travels to Dagobah to
+        train with Jedi Master Yoda, learning the ways of the Force.
+      </p>
+      <button>Show spoiler</button>
+      <p>
+        Meanwhile, Han Solo, Princess Leia, and their allies evade Imperial
+        forces but are ultimately betrayed by Lando Calrissian on Cloud City. In
+        the film's climactic moment, Luke confronts Darth Vader and learns the
+        shocking truth: Vader is his father.
+      </p>
     </>
   );
 }
