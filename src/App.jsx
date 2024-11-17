85f3266c6ba919bbae7b042b0f890aa0f049a055
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-17 19:56:28 +0000

    feat: add warning message for spoilers and handle visibility in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from "react";
 
 function App() {
   const [isSpoilerVisible, setIsSpoilerVisible] = useState(false);
+  const [isWarningVisible, setIsWarningVisible] = useState(true);
 
-  function handleClick() {
+  function handleShowSpoilerClick() {
     setIsSpoilerVisible(true);
+    setIsWarningVisible(false);
+  }
+
+  function handleCloseWarningClick() {
+    setIsWarningVisible(false);
   }
 
   return (
     <>
       <h1>Star Wars: Episode V</h1>
       <h2>Year of production: 1980</h2>
+      <h2>Likes: 0</h2>
+      <button>Like!</button>
       <h2>Storyline:</h2>
+      {isWarningVisible && (
+        <p>
+          Warning! The plot description contains spoilers!
+          <button onClick={handleCloseWarningClick}>X</button>
+        </p>
+      )}
       <p>
         In Star Wars: Episode V - The Empire Strikes Back, the Rebel Alliance
         faces a devastating attack on their base on Hoth by the Empire. Luke
@@ -26,7 +40,7 @@ function App() {
           the shocking truth: Vader is his father.
         </p>
       ) : (
-        <button onClick={handleClick}>Show spoiler</button>
+        <button onClick={handleShowSpoilerClick}>Show spoiler</button>
       )}
     </>
   );
