afc487feec2f4bd18bbf433bc2abb652b71ef41e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-17 20:30:20 +0000

    feat: add like and love buttons with state management for likes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
   const [isSpoilerVisible, setIsSpoilerVisible] = useState(false);
   const [isWarningVisible, setIsWarningVisible] = useState(true);
+  const [numberOfLikes, setNumberOfLikes] = useState(0);
 
   function handleShowSpoilerClick() {
     setIsSpoilerVisible(true);
@@ -13,12 +14,21 @@ function App() {
     setIsWarningVisible(false);
   }
 
+  function handleLikeButtonClick() {
+    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+  }
+
+  function handleLoveButtonClick() {
+    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 3);
+  }
+
   return (
     <>
       <h1>Star Wars: Episode V</h1>
       <h2>Year of production: 1980</h2>
-      <h2>Likes: 0</h2>
-      <button>Like!</button>
+      <h2>Likes: {numberOfLikes}</h2>
+      <button onClick={handleLikeButtonClick}>Like it!</button>
+      <button onClick={handleLoveButtonClick}>Love it!</button>
       <h2>Storyline:</h2>
       {isWarningVisible && (
         <p>
