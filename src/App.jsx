cf31d5ac57fcf179062605c51d5e1e941d8e7a80
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-17 22:45:21 +0000

    feat: extract storyline functionality into a new Plot component

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,13 @@
-import { useState } from "react";
 import { LikesCounter } from "./components/LikesCounter";
+import { Plot } from "./components/Plot";
 
 function App() {
-  const [isSpoilerVisible, setIsSpoilerVisible] = useState(false);
-  const [isWarningVisible, setIsWarningVisible] = useState(true);
-
-  function handleShowSpoilerClick() {
-    setIsSpoilerVisible(true);
-    setIsWarningVisible(false);
-  }
-
-  function handleCloseWarningClick() {
-    setIsWarningVisible(false);
-  }
-
   return (
     <>
       <h1>Star Wars: Episode V</h1>
       <h2>Year of production: 1980</h2>
       <LikesCounter />
-      <h2>Storyline:</h2>
-      {isWarningVisible && (
-        <p>
-          Warning! The plot description contains spoilers!
-          <button onClick={handleCloseWarningClick}>X</button>
-        </p>
-      )}
-      <p>
-        In Star Wars: Episode V - The Empire Strikes Back, the Rebel Alliance
-        faces a devastating attack on their base on Hoth by the Empire. Luke
-        Skywalker, guided by a vision of Obi-Wan Kenobi, travels to Dagobah to
-        train with Jedi Master Yoda, learning the ways of the Force.
-      </p>
-      {isSpoilerVisible ? (
-        <p>
-          Meanwhile, Han Solo, Princess Leia, and their allies evade Imperial
-          forces but are ultimately betrayed by Lando Calrissian on Cloud City.
-          In the film`s climactic moment, Luke confronts Darth Vader and learns
-          the shocking truth: Vader is his father.
-        </p>
-      ) : (
-        <button onClick={handleShowSpoilerClick}>Show spoiler</button>
-      )}
+      <Plot />
     </>
   );
 }
---
+++ src/components/Plot.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export function Plot() {
+  const [isSpoilerVisible, setIsSpoilerVisible] = useState(false);
+  const [isWarningVisible, setIsWarningVisible] = useState(true);
+
+  function handleShowSpoilerClick() {
+    setIsSpoilerVisible(true);
+    setIsWarningVisible(false);
+  }
+
+  function handleCloseWarningClick() {
+    setIsWarningVisible(false);
+  }
+
+  return (
+    <>
+      <h2>Storyline:</h2>
+      {isWarningVisible && (
+        <p>
+          Warning! The plot description contains spoilers!
+          <button onClick={handleCloseWarningClick}>X</button>
+        </p>
+      )}
+      <p>
+        In Star Wars: Episode V - The Empire Strikes Back, the Rebel Alliance
+        faces a devastating attack on their base on Hoth by the Empire. Luke
+        Skywalker, guided by a vision of Obi-Wan Kenobi, travels to Dagobah to
+        train with Jedi Master Yoda, learning the ways of the Force.
+      </p>
+      {isSpoilerVisible ? (
+        <p>
+          Meanwhile, Han Solo, Princess Leia, and their allies evade Imperial
+          forces but are ultimately betrayed by Lando Calrissian on Cloud City.
+          In the film`s climactic moment, Luke confronts Darth Vader and learns
+          the shocking truth: Vader is his father.
+        </p>
+      ) : (
+        <button onClick={handleShowSpoilerClick}>Show spoiler</button>
+      )}
+    </>
+  );
+}
