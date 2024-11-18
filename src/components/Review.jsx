13c8e4ec24409f68a7640c15db9cefda1477ef27
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-18 23:48:27 +0000

    feat: add form for submitting reviews in Review component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,6 +4,21 @@ export function Review() {
   return (
     <>
       <h2>Add review</h2>
+      <form action="">
+        <div>
+          <div>
+            <label htmlFor="author">Author</label>
+          </div>
+          <input type="text" name="author" id="author" />
+        </div>
+        <div>
+          <div>
+            <label htmlFor="">Text</label>
+          </div>
+          <textarea name="text" id="text" placeholder="Your review"></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
