5d7faacf044d7489b495040c3e889fd7f04eb06f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-30 00:01:39 +0000

    feat: integrate Reviews component into App and manage reviews state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { LikesCounter } from "./components/LikesCounter";
 import { Plot } from "./components/Plot";
 import { Form } from "./components/Form";
+import { Reviews } from "./components/Reviews";
+import { useState } from "react";
+
+const initialReviews = [
+  { author: "John", text: "Great film!", id: 1 },
+  { author: "Alice", text: "I don't like it", id: 2 },
+];
 
 function App() {
+  const [reviews, setReviews] = useState(initialReviews);
+
   return (
     <>
       <h1>Star Wars: Episode V</h1>
       <h2>Year of production: 1980</h2>
       <LikesCounter />
       <Plot />
+      <Reviews reviews={reviews} />
       <Form />
     </>
   );
