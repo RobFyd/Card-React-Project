8c2fe6ae3f93484ed1a06950d93151a4e50fe17c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-30 00:01:22 +0000

    feat: modify Reviews component to accept reviews as props instead of using internal state

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,14 +1,6 @@
-import { useState } from "react";
 import { Review } from "./Review";
 
-const initialReviews = [
-  { author: "John", text: "Great film!", id: 1 },
-  { author: "Alice", text: "I don't like it", id: 2 },
-];
-
-export function Reviews() {
-  const [reviews, setReviews] = useState(initialReviews);
-
+export function Reviews({ reviews }) {
   const reviewsElement = reviews.map((r) => (
     <Review key={r.id} author={r.author} text={r.text} />
   ));
