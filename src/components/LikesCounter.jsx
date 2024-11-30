0c962c1bef2d4a5238533bbf4b53213a1a4f69d6
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-30 23:42:44 +0000

    feat: refactor LikesCounter component to accept props for likes handling

--- src/components/LikesCounter.jsx
+++ src/components/LikesCounter.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
-
-export function LikesCounter() {
-  const [numberOfLikes, setNumberOfLikes] = useState(0);
-
-  function handleLikeButtonClick() {
-    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
-  }
-
-  function handleLoveButtonClick() {
-    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 3);
-  }
-
+export function LikesCounter({
+  numberOfLikes,
+  onLikeButtonClick,
+  onLoveButtonClick,
+}) {
   return (
     <>
       <h2>Likes: {numberOfLikes}</h2>
-      <button onClick={handleLikeButtonClick}>Like it!</button>
-      <button onClick={handleLoveButtonClick}>Love it!</button>
+      <button onClick={onLikeButtonClick}>Like it!</button>
+      <button onClick={onLoveButtonClick}>Love it!</button>
     </>
   );
 }
